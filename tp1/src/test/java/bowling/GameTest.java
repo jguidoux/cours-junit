417667a223098491ef02a4913df3254c4f9ffbc4
Briand <jean-philippe.briand@slickteam.fr>
2020-03-06 14:30:19 +0100

    udpate docker

--- tp1/src/test/java/bowling/GameTest.java
+++ tp1/src/test/java/bowling/GameTest.java
@@ -1,15 +1,5 @@
 package bowling;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class GameTest {
 
-    private List<Integer> rollMany(int rollsNumber, int pins) {
-        List<Integer> rolls = new ArrayList<>();
-        for (int i = 0; i < rollsNumber; i++) {
-            rolls.add(pins);
-        }
-        return rolls;
-    }
 }
