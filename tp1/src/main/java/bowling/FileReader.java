b820ac741469039131873af78b67813798b0780a
Briand <jean-philippe.briand@slickteam.fr>
2020-03-06 10:16:29 +0100

    Update TP

--- tp1/src/main/java/bowling/FileReader.java
+++ tp1/src/main/java/bowling/FileReader.java
@@ -2,15 +2,23 @@ package bowling;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.util.Objects;
 
 class FileReader {
 
     static String readClassPathResourceFileContent(String path) {
 
-        File file = new File(path);
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 
         try {
+            URL url = classLoader.getResource(path);
+            if (Objects.isNull(url)) {
+                throw new RuntimeException("Unable to read file");
+            }
+
+            File file = new File(url.getFile());
             return new String(Files.readAllBytes(file.toPath()));
         } catch (IOException e) {
             throw new RuntimeException("Unable to read file");
