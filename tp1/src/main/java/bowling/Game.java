fcda796d82c4a698a6f6153e95c04944ae7190ad
BriandFMarie <briand.marie@live.fr>
2020-03-11 11:36:44 +0100

    add Bowling scoring with tests

--- tp1/src/main/java/bowling/Game.java
+++ tp1/src/main/java/bowling/Game.java
@@ -4,7 +4,40 @@ import java.util.List;
 
 class Game {
 
+    // Calculate scores
     static int score(List<Integer> rolls) {
-        return -1;
+
+        // Initialize variables
+        int ball1 = 0;
+        int ball2 = 0;
+        int ball3 = 0;
+        int totalByRoll = 0;
+        int index = 0;
+        int score = 0;
+
+        // All 10 frames
+        for (int i = 0; i < 10; i++) {
+                // Strike
+                if(rolls.get(index) == 10) {
+                    ball1 = rolls.get(index);
+                    ball2 = rolls.get(index+1);
+                    ball3 = rolls.get(index+2);
+                    totalByRoll = ball1 + ball2 + ball3;
+                    index++;
+
+                } else {
+                    ball1 = rolls.get(index);
+                    ball2 = rolls.get(index+1);
+                    totalByRoll = ball1 + ball2;
+
+                    //Spare
+                    if(totalByRoll == 10) {
+                        totalByRoll += rolls.get(index+2);
+                    }
+                    index+=2;
+                }
+            score += totalByRoll;
+        }
+        return score;
     }
 }
--- tp1/src/test/java/bowling/GameTest.java
+++ tp1/src/test/java/bowling/GameTest.java
@@ -1,5 +1,52 @@
 package bowling;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.*;
+
 class GameTest {
 
+    @Test
+    public void should_return_score_with_zero() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.addAll(asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        Integer score = Game.score(rolls);
+        assertEquals(0, score);
+    }
+
+    @Test
+    public void should_return_score_with_spare() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.addAll(asList(0, 0, 0, 0, 7, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        Integer score = Game.score(rolls);
+        assertEquals(14, score);
+    }
+
+    @Test
+    public void should_return_score_with_strike() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.addAll(asList(0, 0, 0, 0, 10, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        Integer score = Game.score(rolls);
+        assertEquals(20, score);
+    }
+
+    @Test
+    public void should_return_score_like_main() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.addAll(asList(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6));
+        Integer score = Game.score(rolls);
+        assertEquals(133, score);
+    }
+
+    @Test
+    public void should_return_score_always_strike() {
+        List<Integer> rolls = new ArrayList<>();
+        rolls.addAll(asList(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10));
+        Integer score = Game.score(rolls);
+        assertEquals(300, score);
+    }
 }
